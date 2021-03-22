ebf94966f1afb4443a089fad3acd5c183509ccdb
as3ii <as3ii777@gmail.com>
2021-03-22 01:29:39 +0100

    qjackctl: updated jackd settings, updated patchbay, improved post-startup script

--- rncbc.org/post-startup.sh
+++ rncbc.org/post-startup.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/sh
 
-alsa_in -d hw:Loopback,1 -j "loopout" -p256 -n3 -c2 -r48000 >/dev/null&
-alsa_out -d hw:PCH -j "PCH" -p256 -n3 -c2 -r48000 >/dev/null&
+# set jackd scheduler
+chrt -apf 40 "$(pidof jackd)"
+
+#chrt -f 20 alsa_in -d hw:Loopback,1 -j "loopout" -p256 -n3 -c2 -r48000 >/dev/null&
+#chrt -f 20 alsa_out -d hw:PCH -j "PCH" -p256 -n3 -c2 -r48000 >/dev/null&
 while pgrep "jackd" >/dev/null; do
-    pgrep -x "non-mixer" >/dev/null || non-mixer /home/as3ii/.config/ntk/mixer
+    pgrep -x "non-mixer" >/dev/null || \
+        chrt -f 10 non-mixer /home/as3ii/.config/ntk/mixer
     sleep 2
 done &
