4f5df183c4f3baad113ee41d7b21f6403b96947b
as3ii <as3ii777@gmail.com>
2021-05-12 23:41:37 +0200

    mpv: fixed autosub, enabled more time seek keybindings

--- mpv/scripts/autosub.lua
+++ mpv/scripts/autosub.lua
@@ -3,7 +3,7 @@
 --=============================================================================
 --          This script uses Subliminal to download subtitles,
 --          so make sure to specify your system's Subliminal location below:
-local subliminal = '/home/as3ii/.local/bin/sublominal'
+local subliminal = '/home/as3ii/.local/bin/subliminal'
 --=============================================================================
 -->>    SUBTITLE LANGUAGE:
 --=============================================================================
