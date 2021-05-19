f44ecd8c330977f4e3debb1283e05559a803c8cb
as3ii <as3ii777@gmail.com>
2021-05-20 00:43:42 +0200

    updated qjackctl config, used now only for patchbay and session management

--- rncbc.org/post-startup.sh
+++ rncbc.org/post-startup.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/sh
 
 # set jackd scheduler
-chrt -apf 40 "$(pidof jackd)"
+#chrt -apf 80 "$(pidof jackd)"
 
 #chrt -f 20 alsa_in -d hw:Loopback,1 -j "loopout" -p256 -n3 -c2 -r48000 >/dev/null&
 #chrt -f 20 alsa_out -d hw:PCH -j "PCH" -p256 -n3 -c2 -r48000 >/dev/null&
-while pgrep "jackd" >/dev/null; do
+while pgrep "jackdbus" >/dev/null; do
     pgrep -x "non-mixer" >/dev/null || \
-        chrt -f 10 non-mixer /home/as3ii/.config/ntk/mixer
+        chrt -f 70 non-mixer /home/as3ii/.config/ntk/mixer
     sleep 2
 done &
